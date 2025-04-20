27d610ff7f1a9c5e3e6e246927ca391269b76036
Jose Manuel Jimenez <jmjimenezg@gmail.com>
2025-04-20 10:52:47 +0000

    Add updated module chapter 02

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,6 @@ import { AppService } from './app.service';
 import CustomConfigLoader from './custom-config/custom-config.service';
 import { ConfigModule } from '@nestjs/config';
 import { OllamaService } from './ollama/ollama.service';
-import { DataProcessingService } from './data-processing/data-processing.service';
-import { DatabaseService } from './database/database.service';
-import { RagService } from './rag/rag/rag.service';
-import { RetrieverService } from './rag/retriever/retriever.service';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -16,6 +12,6 @@ import { RetrieverService } from './rag/retriever/retriever.service';
     })
   ],
   controllers: [AppController],
-  providers: [AppService, OllamaService, DataProcessingService, DatabaseService, RagService, RetrieverService],
+  providers: [AppService, OllamaService],
 })
-export class AppModule {}
+export class AppModule { }
